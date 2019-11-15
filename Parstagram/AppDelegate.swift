c026e9f155e012e25fb3142b8e7f53cb6c80b37c
Diana Balderas <balderasdiana27@gmail.com>
2019-11-14 19:20:28 -0800

    :)

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Nebala. All rights reserved.
 //
 
+import Parse
 import UIKit
 
 @UIApplicationMain
@@ -15,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(
+                   with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                       configuration.applicationId = "Parstagram"
+                       configuration.server = "https://secure-garden-97690.herokuapp.com/parse"
+                   })
+               )
+        
         return true
     }
 
